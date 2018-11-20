5c9192eb5236adcc3548ad822738853f2797f77e
UnterKatze <44979562+UnterKatze@users.noreply.github.com>
2018-11-20 16:48:28 +0100

    v2

--- UselessBox.ino
+++ UselessBox.ino
@@ -1,35 +1,52 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include <Wire.h>
+#include <Servo.h>
 #include <LiquidCrystal_I2C.h>
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  #include "Wire.h"
+#include "Wire.h"
 #endif
 
 MPU6050 accelgyro;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+Servo servo1;
+Servo servo2;
 
-int delayTime = 333;
+int delayTime = 250;
 int16_t xAxis, yAxis, zAxis;
 int xResult, yResult, zResult;
 double xOffset = 0, yOffset = 0, zOffset = 0;
 double xRange = 16300.0, yRange = 16300.0, zRange = 32600.0;
 double xMaxAngle = 90.0, yMaxAngle = 90.0, zMaxAngle = 180.0;
 int GroundSwitch = 7;
+int FunctionSwitch = 8;
+int servo1StartPos = 0;
+int servo2StartPos = 0;
+int maxServo1Angle = 60;
+int servo2TriggerAngle = 40;
+int servo2SwitchHit = 45;
+int servo1Pin = 3;
+int servo2Pin = 5;
+
 
 #define OUTPUT_READABLE_ACCELGYRO
 
 void setup()
 {
   pinMode(GroundSwitch, OUTPUT);
+  pinMode(FunctionSwitch, INPUT);
   digitalWrite(GroundSwitch, LOW);
-  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup(400, true);
-  #endif
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
 
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
+  servo1.write(servo1StartPos);
+  servo2.write(servo2StartPos);
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
@@ -56,30 +73,74 @@ void setup()
 
 void loop()
 {
-  accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
+  if (FunctionSwitch == HIGH)
+  {
 
-  lcd.setCursor(0, 0);
-  lcd.print("x: ");
-  xResult = ((xAxis - xOffset) / xRange) * xMaxAngle;
-  lcd.print(xResult);
+    for (int i = 0; i <= maxServo1Angle; i + 2)
+    {
+      accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
+      xResult = ((xAxis - xOffset) / xRange) * xMaxAngle;
+      yResult = ((yAxis - yOffset) / yRange) * yMaxAngle;
+      zResult = ((zAxis - zOffset) / zRange) * zMaxAngle;
+      
+      if (i % 10 == 0)
+      {
+        resetDisplay();
+        lcd.setCursor(0, 0);
+        lcd.print("x: "); 
+        lcd.print(xResult);
 
-  lcd.setCursor(8, 0);
-  lcd.print("y: ");
-  yResult = ((yAxis - yOffset) / yRange) * yMaxAngle;
-  lcd.print(yResult);
+        lcd.setCursor(8, 0);
+        lcd.print("y: "); 
+        lcd.print(yResult);
 
-  lcd.setCursor(0, 1);
-  lcd.print("z: ");
-  zResult = ((zAxis - zOffset) / zRange) * zMaxAngle;
-  lcd.print(zResult);
+        lcd.setCursor(0, 1);
+        lcd.print("z: ");
+        lcd.print(zResult);
+      }
 
-  delay(delayTime);
-  resetDisplay();
+      servo1.write(i);
+      delay(50);
 
+      if (xResult >= servo2TriggerAngle)
+      {
+        servo2.write(servo2SwitchHit);
+        delay(333);
+        servo2.write(servo2StartPos);
+      }
+      
+    }
+
+    for (int j = maxServo1Angle; j == 0; j - 2)
+    {
+      accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
+      xResult = ((xAxis - xOffset) / xRange) * xMaxAngle;
+      yResult = ((yAxis - yOffset) / yRange) * yMaxAngle;
+      zResult = ((zAxis - zOffset) / zRange) * zMaxAngle;
+
+      if (j % 10 == 0)
+      {
+        resetDisplay();
+        lcd.setCursor(0, 0);
+        lcd.print("x: ");  
+        lcd.print(xResult);
+
+        lcd.setCursor(8, 0);
+        lcd.print("y: ");   
+        lcd.print(yResult);
+
+        lcd.setCursor(0, 1);
+        lcd.print("z: ");  
+        lcd.print(zResult);
+      }
+      
+      servo1.write(j);
+      delay(50);     
+    }
+  }
 }
 
-void resetDisplay (void) 
-{
+void resetDisplay (void) {
   lcd.setCursor(0, 0);
   lcd.println("                ");
   lcd.setCursor(0, 1);
