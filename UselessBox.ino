1fc183b6d94799ecb862d26c8e1a46aa903aa4ec
UnterKatze <44979562+UnterKatze@users.noreply.github.com>
2018-11-20 19:38:24 +0100

    v3

--- UselessBox.ino
+++ UselessBox.ino
@@ -13,7 +13,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servo1;
 Servo servo2;
 
-int delayTime = 250;
+int delayTime = 50;
 int16_t xAxis, yAxis, zAxis;
 int xResult, yResult, zResult;
 double xOffset = 0, yOffset = 0, zOffset = 0;
@@ -23,12 +23,12 @@ int GroundSwitch = 7;
 int FunctionSwitch = 8;
 int servo1StartPos = 0;
 int servo2StartPos = 0;
-int maxServo1Angle = 60;
-int servo2TriggerAngle = 40;
-int servo2SwitchHit = 45;
+int maxServo1Angle = 90;
+int servo2TriggerAngle = 45;
+int servo2SwitchHit = 90;
 int servo1Pin = 3;
 int servo2Pin = 5;
-
+int i, j;
 
 #define OUTPUT_READABLE_ACCELGYRO
 
@@ -52,20 +52,26 @@ void setup()
   lcd.setCursor(0, 0);
   lcd.print("Updating Offsets");
   accelgyro.initialize();
-  delay(1000);
+  delay(250);
+  lcd.setCursor(0, 1);
 
-  for (int i = 1; i <= 100; i++)
+  for (int k = 1; k <= 96; k++)
   {
     accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
     xOffset = xOffset + xAxis;
     yOffset = yOffset + yAxis;
     zOffset = zOffset + zAxis;
+    if (k % 6 == 0)
+    {
+      lcd.print(">");
+    }
     delay(50);
   }
 
-  xOffset = xOffset / 100.0;
-  yOffset = yOffset / 100.0;
-  zOffset = zOffset / 100.0;
+  xOffset = xOffset / 96.0;
+  yOffset = yOffset / 96.0;
+  zOffset = zOffset / 96.0;
+
   resetDisplay();
   digitalWrite(GroundSwitch, HIGH);
 }
@@ -73,71 +79,83 @@ void setup()
 
 void loop()
 {
-  if (FunctionSwitch == HIGH)
+  if (digitalRead(FunctionSwitch))
   {
 
-    for (int i = 0; i <= maxServo1Angle; i + 2)
+    for (i = 0; i <= maxServo1Angle; i = i + 2)
     {
       accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
       xResult = ((xAxis - xOffset) / xRange) * xMaxAngle;
       yResult = ((yAxis - yOffset) / yRange) * yMaxAngle;
       zResult = ((zAxis - zOffset) / zRange) * zMaxAngle;
-      
+
       if (i % 10 == 0)
       {
         resetDisplay();
         lcd.setCursor(0, 0);
-        lcd.print("x: "); 
+        lcd.print("    ");
         lcd.print(xResult);
+        lcd.print(" Grad");
 
-        lcd.setCursor(8, 0);
-        lcd.print("y: "); 
-        lcd.print(yResult);
-
-        lcd.setCursor(0, 1);
-        lcd.print("z: ");
-        lcd.print(zResult);
+        //lcd.setCursor(8, 0);
+        //lcd.print("y: ");
+        //lcd.print(yResult);
+        //
+        //lcd.setCursor(0, 1);
+        //lcd.print("z: ");
+        //lcd.print(zResult);
       }
 
       servo1.write(i);
-      delay(50);
+      delay(delayTime);
 
       if (xResult >= servo2TriggerAngle)
       {
-        servo2.write(servo2SwitchHit);
-        delay(333);
-        servo2.write(servo2StartPos);
-      }
-      
-    }
-
-    for (int j = maxServo1Angle; j == 0; j - 2)
-    {
-      accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
-      xResult = ((xAxis - xOffset) / xRange) * xMaxAngle;
-      yResult = ((yAxis - yOffset) / yRange) * yMaxAngle;
-      zResult = ((zAxis - zOffset) / zRange) * zMaxAngle;
-
-      if (j % 10 == 0)
-      {
         resetDisplay();
         lcd.setCursor(0, 0);
-        lcd.print("x: ");  
-        lcd.print(xResult);
+        lcd.print("   Top Opened   ");
 
-        lcd.setCursor(8, 0);
-        lcd.print("y: ");   
-        lcd.print(yResult);
+        servo2.write(servo2SwitchHit);
+        delay(1000);
+        servo2.write(servo2StartPos);
 
-        lcd.setCursor(0, 1);
-        lcd.print("z: ");  
-        lcd.print(zResult);
+        for (j = i; j >= 0; j = j - 2)
+        {
+          accelgyro.getAcceleration(&xAxis, &yAxis, &zAxis);
+          xResult = ((xAxis - xOffset) / xRange) * xMaxAngle;
+          yResult = ((yAxis - yOffset) / yRange) * yMaxAngle;
+          zResult = ((zAxis - zOffset) / zRange) * zMaxAngle;
+
+          if (j % 10 == 0)
+          {
+            resetDisplay();
+            lcd.setCursor(0, 0);
+            lcd.print("    ");
+            lcd.print(xResult);
+            lcd.print(" Grad");
+
+            //lcd.setCursor(8, 0);
+            //lcd.print("y: ");
+            //lcd.print(yResult);
+            //
+            //lcd.setCursor(0, 1);
+            //lcd.print("z: ");
+            //lcd.print(zResult);
+          }
+          servo1.write(j);
+          delay(delayTime);
+        }
+        break;
+        resetDisplay();
       }
-      
-      servo1.write(j);
-      delay(50);     
     }
+    resetDisplay();
   }
+
+  lcd.setCursor(0, 0);
+  lcd.print("    Control");
+  lcd.setCursor(0, 1);
+  lcd.print("    Switch");
 }
 
 void resetDisplay (void) {
